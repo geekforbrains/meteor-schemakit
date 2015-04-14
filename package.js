e689f8cc190560a304eaf31b3c0d5a3ecdff87bf
Gavin Vickery <gavin@geekforbrains.com>
2015-04-13 18:39:28 -0700

    Bumping version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'geekforbrains:schemakit',
-  version: '0.0.3',
+  version: '0.0.4',
   summary: 'A dead-easy package for validating schemas in Meteor.',
   git: 'https://github.com/geekforbrains/meteor-schemakit',
   documentation: 'README.md'
